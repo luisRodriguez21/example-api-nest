616f37b43b9072449d638787432c24928b3b6da1
luisRM <luis.rodriguez.mateos21@gmail.com>
2024-08-10 11:25:44 -0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/app.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: "pokemonsdb",
+    }),
+
+    PokemonModule,
+
+    CommonModule,
+
+    SeedModule,
+  ],
+})
+export class AppModule { }
